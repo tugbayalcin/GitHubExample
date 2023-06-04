f7f54e3c8a4b43102b84730ce0aced05bb9a36dc
DuyguTo <duygutozanca@gmail.com>
2023-06-04 15:29:42 -0400

    duygunun commiti

---
+++ src/test/java/GithubDuygu.java
@@ -0,0 +1,7 @@
+public class GithubDuygu {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("duygu");
+        System.out.println("proje");
+    }
+}
